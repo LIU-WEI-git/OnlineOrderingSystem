302eaf9f9c20bfe640e4d325bfbcbc388b31b823
neillythz <neillythz@163.com>
2019-11-27 04:16:44 +0800

    基本完成管理员订单操作（设置订单状态、配送状态、取三种状态的订单、计算订单数量、计算收入）

---
+++ src/ordering/repository/OrderRepository.java
@@ -0,0 +1,152 @@
+package ordering.repository;
+
+import ordering.domain.Order;
+
+import java.util.List;
+
+
+public interface OrderRepository {
+
+    /**
+     * 查看查询顾客是否有订单记录
+     *
+     * @param custome_account
+     * @return
+     */
+    boolean isCustomerInDB(String custome_account);
+
+    /**
+     * 查看顾客历史订单数量
+     *
+     * @param customer_account
+     * @return
+     */
+    int getCustomerTotalOrders(String customer_account);
+
+    /**
+     * 获取customer的详细订单
+     *
+     * @param customer_account
+     * @return
+     */
+    List<Order> getCustomerOrders(String customer_account);
+
+    /**
+     * 获取所有订单数目
+     * @return
+     */
+    int getTotalOrder();
+
+    /**
+     * 获取所有订单（按时间排序）
+     * @return
+     */
+    List<Order> getOrders();
+
+    /**
+     * 通过order_id删除一条订单记录
+     * @param order_id
+     * @return
+     */
+    boolean deleteOrder(String order_id);
+
+    /**
+     * 通过order_id获取一条订单记录
+     * @param order_id
+     * @return
+     */
+    Order getOrder(String order_id);
+
+    /**
+     * 更新一条订单记录
+     * @param order
+     * @return
+     */
+    boolean resetOrder(Order order);
+
+    /**
+     * 添加一条订单记录
+     * @param order
+     * @return
+     */
+    boolean addOrder(Order order);
+
+    /**
+     * 确认订单,尚未配送
+     *
+     * @param order
+     */
+    void confirmOrder(Order order);
+
+    /**
+     * 订单开始配送
+     *
+     * @param order
+     */
+    void confirmDelivery(Order order);
+
+    /**
+     * 完成配送
+     *
+     * @param order
+     */
+    void completeDelivery(Order order);
+
+    /**
+     * 完成订单
+     *
+     * @param order
+     */
+    void completeOrder(Order order);
+
+    /**
+     * 获取已确认但未配送的订单
+     *
+     * @return
+     */
+    List<Order> getConfirmedAndUndeliveredOrder();
+
+    /**
+     * 获取正在配送的订单
+     *
+     * @return
+     */
+    List<Order> getDeliveringOrder();
+
+    /**
+     * 获取已完成的订单
+     *
+     * @return
+     */
+    List<Order> getCompletedOrder();
+
+    /**
+     * 获取某天已完成的订单数（日期格式：yyyy-MM-dd，类型为string）
+     *
+     * @param day
+     * @return
+     */
+    long getTotalCompletedOrdersByDayNum(String day);
+
+    /**
+     * 获取所有已完成订单数
+     *
+     * @return
+     */
+    long getTotalCompletedOrdersNum();
+
+    /**
+     * 获取某日收入总计（日期格式：yyyy-MM-dd，类型为string）
+     *
+     * @return
+     */
+    double getTotalIncomeByDay(String day);
+
+    /**
+     * 获取总收入
+     *
+     * @return
+     */
+    double getTotalIncome();
+
+}
---
+++ src/ordering/repository/jdbc/JdbcOrderRepository.java
@@ -0,0 +1,163 @@
+package ordering.repository.jdbc;
+
+import ordering.domain.Order;
+import ordering.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class JdbcOrderRepository implements OrderRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    private RowMapper<Order> orderRowMapper=new BeanPropertyRowMapper<Order>(Order.class);
+
+    private static final String TOTAL_ORDERS="select count(*) from `order` where customer_account = ";
+    private static final String CUSTOMER_ORDERS="select * from `order` where customer_account = ";
+
+    // 确认订单
+    private static final String CONFIRM_ORDER = "UPDATE `order` SET order_state=1 WHERE order_id=?";
+    // 完成订单
+    private static final String COMPLETE_ORDER = "UPDATE `order` SET order_state=2 WHERE order_id=?";
+    // 确认开始配送
+    private static final String CONFIRM_DELIVERY = "UPDATE `order` SET delivery_state=1 WHERE order_id=?";
+    // 完成配送
+    private static final String COMPLETE_DELIVERY = "UPDATE `order` SET delivery_state=2 WHERE order_id=?";
+
+    // order表查询
+    private static final String SELECT_ORDER = "SELECT * FROM `order`";
+    // 排序
+    private static final String ORDER_BY = " ORDER BY create_time DESC";
+    // 查询已确认但未配送的订单
+    private static final String SELECT_CONFIRMED_UNDELIVERED_ORDER = SELECT_ORDER + " WHERE order_state=1 AND delivery_state=0";
+    // 查询正在配送的订单
+    private static final String SELECT_DELIVERING_ORDER = SELECT_ORDER + " WHERE order_state=1 AND delivery_state=1";
+    // 查询已完成的订单（逆序）
+    private static final String SELECT_COMPLETED_ORDER = SELECT_ORDER + " WHERE order_state=2 AND delivery_state=2";
+
+    // 获取已完成订单总数
+    private static final String SELECT_COMPLETED_COUNT = "SELECT COUNT(*) FROM `order` WHERE order_state=2 AND delivery_state=2";
+    // 获取收入
+    private static final String TOTAL_INCOME = "SELECT COALESCE(SUM(v.per_income), 0) AS income FROM (SELECT order_id, create_time, (order_price + discount) AS per_income FROM `order` WHERE order_state=2 AND delivery_state=2) AS v";
+
+    @Autowired
+    public JdbcOrderRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
+    }
+    @Override
+    public boolean isCustomerInDB(String customer_account) {
+        if(jdbcTemplate.queryForObject(TOTAL_ORDERS+customer_account,Integer.class)==0)
+            return false;
+        else return true;
+    }
+
+    @Override
+    public int getCustomerTotalOrders(String customer_account) {
+        return jdbcTemplate.queryForObject(TOTAL_ORDERS+customer_account,Integer.class);
+    }
+
+    @Override
+    public List<Order> getCustomerOrders(String customer_account) {
+        return jdbcTemplate.query(CUSTOMER_ORDERS+"'"+customer_account+"' order by create_time DESC ",orderRowMapper);
+    }
+
+    @Override
+    public int getTotalOrder() {
+        return jdbcTemplate.queryForObject("select count(*) from `order` ",Integer.class);
+    }
+
+    @Override
+    public List<Order> getOrders() {
+        return jdbcTemplate.query("select * from `order` ",orderRowMapper);
+    }
+
+    @Override
+    public boolean deleteOrder(String order_id) {
+        jdbcTemplate.update("delete from `order` where order_id ="+order_id);
+        return true;
+    }
+
+    @Override
+    public Order getOrder(String order_id) {
+        return jdbcTemplate.queryForObject("select * from `order` where order_id = "+order_id,orderRowMapper);
+    }
+
+    @Override
+    public boolean resetOrder(Order order) {
+        jdbcTemplate.update("update `order` set customer_account=?,address_id=?," +
+                "create_time=?,remark=?,order_state=?,delivery_state=?,discount=?,order_price=? where order_id =?",
+                order.getCustomer_account(),order.getAddress_id(),order.getCreate_time(),order.getRemark(),
+                order.getOrder_state(),order.getDelivery_state(),order.getDiscount(),order.getOrder_price());
+        return true;
+    }
+
+    @Override
+    public boolean addOrder(Order order) {
+        jdbcTemplate.update("insert into `order` (order_id, customer_account, address_id, create_time, remark, order_state, delivery_state, discount, order_price) VALUE "
+                            +"(?,?,?,?,?,?,?,?,?)",order.getOrder_id(),order.getCustomer_account(),order.getAddress_id(),
+                            order.getCreate_time(),order.getRemark(),order.getOrder_state(),order.getDelivery_state(),order.getDiscount(),
+                            order.getOrder_price());
+        return true;
+    }
+
+    @Override
+    public void confirmOrder(Order order) {
+        jdbcTemplate.update(CONFIRM_ORDER, order.getOrder_id());
+    }
+
+    @Override
+    public void confirmDelivery(Order order) {
+        jdbcTemplate.update(CONFIRM_DELIVERY, order.getOrder_id());
+    }
+
+    @Override
+    public void completeDelivery(Order order) {
+        jdbcTemplate.update(COMPLETE_DELIVERY, order.getOrder_id());
+    }
+
+    @Override
+    public void completeOrder(Order order) {
+        jdbcTemplate.update(COMPLETE_ORDER, order.getOrder_id());
+    }
+
+    @Override
+    public List<Order> getConfirmedAndUndeliveredOrder() {
+        return jdbcTemplate.query(SELECT_CONFIRMED_UNDELIVERED_ORDER, orderRowMapper);
+    }
+
+    @Override
+    public List<Order> getDeliveringOrder() {
+        return jdbcTemplate.query(SELECT_DELIVERING_ORDER, orderRowMapper);
+    }
+
+    @Override
+    public List<Order> getCompletedOrder() {
+        return jdbcTemplate.query(SELECT_COMPLETED_ORDER + ORDER_BY, orderRowMapper);
+    }
+
+    @Override
+    public long getTotalCompletedOrdersByDayNum(String day) {
+        return jdbcTemplate.queryForObject(SELECT_COMPLETED_COUNT + " AND create_time LIKE '" + day + "%'", long.class);
+    }
+
+    @Override
+    public long getTotalCompletedOrdersNum() {
+        return jdbcTemplate.queryForObject(SELECT_COMPLETED_COUNT, long.class);
+    }
+
+    @Override
+    public double getTotalIncomeByDay(String day) {
+        return jdbcTemplate.queryForObject(TOTAL_INCOME + " WHERE v.create_time LIKE '" + day + "%'", double.class);
+    }
+
+    @Override
+    public double getTotalIncome() {
+        return jdbcTemplate.queryForObject(TOTAL_INCOME, double.class);
+    }
+
+
+}
